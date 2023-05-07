a05acb3227d838cbeddad5a183b855a52a35eee3
Tomoharu Kawakubo <tomo.kawakubo@gmail.com>
2023-05-07 13:59:33 +0900

    2023年05月07日授業分

--- JavaScriptHonkaku/yasumatsu-niwa/drag_and_drop/sample2.js
+++ JavaScriptHonkaku/yasumatsu-niwa/drag_and_drop/sample2.js
@@ -1,9 +1,11 @@
 window.addEventListener('DOMContentLoaded', event => {
     const target = document.getElementById('target');
     const show_case = document.getElementById('show-case');
-    const total = document.getElementById('total');
+    const table_items = document.getElementById('items');
+    const grand_total = document.getElementById('grand-total');
     const dragstart_handler = event => {
         event.dataTransfer.setData("text", event.target.id);
+        console.log(`event.target.id: ${event.target.id}`);
         event.dataTransfer.effectAllowed = "move";
     };
     const dragover_handler = event => {
@@ -28,6 +30,7 @@ window.addEventListener('DOMContentLoaded', event => {
         itemNameTd.appendChild(itemNameText);
         tr.appendChild(itemNameTd);
         const unitPriceTd = document.createElement('td');
+        unitPriceTd.className = 'unit-price';
         const unitPriceText = document.createTextNode(item.querySelector('.unit-price').textContent);
         const unitPrice = extract_int(unitPriceText.textContent);        
         unitPriceTd.appendChild(unitPriceText);
@@ -35,8 +38,11 @@ window.addEventListener('DOMContentLoaded', event => {
         const quantityTd = document.createElement('input');
         quantityTd.type = 'number';
         quantityTd.value = 1;
+        quantityTd.min = 1;
+        quantityTd.className = 'update-quantity';
         tr.appendChild(quantityTd);
         const subTotalTd = document.createElement('td');
+        subTotalTd.className = 'subtotal';
         const subTotalText = document.createTextNode(unitPrice * Number.parseInt(quantityTd.value));
         subTotalTd.appendChild(subTotalText);
         tr.appendChild(subTotalTd);
@@ -48,8 +54,27 @@ window.addEventListener('DOMContentLoaded', event => {
         const data = event.dataTransfer.getData("text");
         const item = document.getElementById(data);
         event.target.appendChild(item);
-        total.appendChild(create_tr(item));
+        table_items.appendChild(create_tr(item));
+        update_grand_total();
     };
+    const update_grand_total = event => {
+        const subtotals = document.querySelectorAll('.subtotal');
+        let sum = 0;
+        for (const subtotal of subtotals) {
+            sum += Number.parseInt(subtotal.textContent);
+        }
+        grand_total.textContent = sum;
+    };
+    const update_quantity = event => {
+        const quantity_button = event.target;
+        const parentTd = quantity_button.parentNode;
+        const unit_price = parentTd.querySelector('.unit-price');
+        const subtotal = parentTd.querySelector('.subtotal');
+        subtotal.textContent = extract_int(unit_price.textContent) * quantity_button.value;
+        update_grand_total();
+    };
+    table_items.addEventListener('change', update_quantity);
+
     const items = document.getElementsByClassName('item');
     for (const item of items) {
         item.addEventListener('dragstart', dragstart_handler);
