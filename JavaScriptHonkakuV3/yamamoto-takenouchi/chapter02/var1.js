aa901829419ece37fbac2581017a11b13016069e
Tomoharu Kawakubo <tomo.kawakubo@gmail.com>
2023-06-20 20:58:32 +0900

    2023年06月20日授業分

---
+++ JavaScriptHonkakuV3/yamamoto-takenouchi/chapter02/var1.js
@@ -0,0 +1,18 @@
+let name = '山田太郎';
+let title = 'JavaScript本格入門';
+/*
+長いコード
+*/
+let product_name = '洗濯機';
+console.log(name + 'さん、こんにちは！')
+
+let x = 1000; 
+x = 'ABC';
+x = true;
+x = function() { console.log(name); }
+x();
+x = 20;
+console.log(x);
+
+weight = 70;
+console.log('体重は' + weight + 'kgです。');
