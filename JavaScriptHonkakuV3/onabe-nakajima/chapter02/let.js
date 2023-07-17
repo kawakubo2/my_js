cf962fbc45d71d475d9be74641965bf74b2f32ca
Tomoharu Kawakubo <tomo.kawakubo@gmail.com>
2023-07-17 18:11:32 +0900

    2023年07月17日授業分

---
+++ JavaScriptHonkakuV3/onabe-nakajima/chapter02/let.js
@@ -0,0 +1,11 @@
+let msg = 'こんにちは、JavaScript！！';
+console.log(msg);
+
+let msg2 = 'Hello World!';
+console.log(msg2);
+
+let 挨拶 = 'こんにちは、山田さん';
+console.log(挨拶);
+
+let message = 'この値は不正です。';
+console.log(message);
