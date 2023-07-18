f80912bc3f1549f33f31b8b35061fe9f27f328d1
Tomoharu Kawakubo <tomo.kawakubo@gmail.com>
2023-07-18 16:59:16 +0900

    2023年07月18日授業分

---
+++ JavaScriptHonkakuV3/donnelly/chapter02/encoding.js
@@ -0,0 +1,4 @@
+let name = '川久保智晴';
+for (let i = 0; i < name.length; i++) {
+    console.log(name.charAt(i) + 'の文字コードは' + name.codePointAt(i));
+}
---
+++ JavaScriptHonkakuV3/donnelly/chapter02/let.js
@@ -0,0 +1,11 @@
+let message = 'こんにちは、JavaScript！！'; // 変数宣言
+console.log(message);
+message = 'こんにちは、HTML！！';
+console.log(message);
+
+// 変数 ---> variable
+
+let names = ['山田', '田中', '鈴木', '佐藤'];
+for (let name of names) {
+    console.log(name + 'さん、こんにちは！\n今日は2023年08月号のご案内です。');
+}
