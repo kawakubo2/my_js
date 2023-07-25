e1dc8da6e761f1263440da2a63cb478453500729
Tomoharu Kawakubo <tomo.kawakubo@gmail.com>
2023-07-25 16:59:55 +0900

    2023年07月25日授業分

---
+++ JavaScriptHonkakuV3/donnelly/chapter02/list.js
@@ -0,0 +1,12 @@
+let data = ['JavaScript', 'Python', 'PHP', 'Ruby', 'Perl'];
+console.log(data[0]);
+console.log(data[1]);
+console.log(data[2]);
+console.log(data[3]);
+console.log(data[4]);
+
+// 繰り返し構文(for, while, for in, for of)
+console.log('---------');
+for (const d of data) { // d --- 仮変数
+    console.log(d);
+}
---
+++ JavaScriptHonkakuV3/donnelly/chapter02/var1.js
@@ -0,0 +1,6 @@
+let name = 'Taro Yamada';
+console.log(typeof name);
+name = 123;
+console.log(typeof name);
+name = true;
+console.log(typeof name);
