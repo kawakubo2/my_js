9428fd5c520272d09c31c012cecb76d4f2f21c33
Tomoharu Kawakubo <tomo.kawakubo@gmail.com>
2023-07-25 21:58:23 +0900

    2023年07月25日授業分

---
+++ JavaScriptHonkakuV3/yamamoto-takenouchi/chapter05/equals.js
@@ -0,0 +1,14 @@
+let x = 0.2;
+let y = 0.6;
+
+if (x * 10 * 3 === y * 10) {
+    console.log('等しい');
+} else {
+    console.log('等しくない');
+}
+
+if (Math.abs(x * 3 - y) < Number.EPSILON) {
+    console.log('等しい');
+} else {
+    console.log('等しくない');
+}
