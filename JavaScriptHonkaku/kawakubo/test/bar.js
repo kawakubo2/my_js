e351a043dd850e3218f820f3e2945feb3c70e492
Tomoharu Kawakubo <tomo.kawakubo@gmail.com>
2024-02-13 17:31:40 +0900

    2024年02月13日予習分

---
+++ JavaScriptHonkaku/kawakubo/test/bar.js
@@ -0,0 +1,5 @@
+import add from './foo.js';
+export default function sub(x, y) {
+    return x - y;
+}
+console.log(add(100, 300));
---
+++ JavaScriptHonkaku/kawakubo/test/foo.js
@@ -0,0 +1,5 @@
+import sub from './bar.js';
+export default function add(x, y) {
+    return x + y;
+}
+console.log(sub(100, 60));
