afd482d44f004f99a95b2d19c3080443f5a46c6d
Tomoharu Kawakubo <tomo.kawakubo@gmail.com>
2024-03-20 00:05:17 +0900

    2024年03月19日授業分

--- JavaScriptHonkakuV3/kai-kobayashi/chapter04/Q2.js
+++ JavaScriptHonkakuV3/kai-kobayashi/chapter04/Q2.js
@@ -1,13 +1,20 @@
-const fruits = ['apple', 'banana', 'apple', 'orange', 'banana', 'grape',
-                'banana', 'orange', 'pear', 'grape', 'apple', 'apple', 'banana']
-
-const fruits_counter = {'apple': 0, 'banana': 0, 'orange': 0, 'grape': 0,
-                        'pear': 0};
-
+const fruits = ["apple", "banana", "apple", "orange", "banana", "grape", "banana", "orange", "pear", "grape", "apple", "apple", "banana"]
+const fruits_counter = {};
 for (const fruit of fruits) {
-    fruits_counter[fruit] = fruits_counter[fruit] + 1;
+    if (fruits_counter[fruit]) {
+        fruits_counter[fruit] = fruits_counter[fruit] + 1;
+    } else {
+        fruits_counter[fruit] = 1;
+    }
 }
 
 for (const fruit in fruits_counter) {
     console.log(`${fruit}: ${fruits_counter[fruit]}`)
 }
+
+const vegetables = {}
+vegetables['pumpkin'] = 150;
+vegetables['cucumber'] = 130;
+vegetables['pumpkin'] = 200;
+console.log(vegetables);
+
