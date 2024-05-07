74c59186df3056e1e05ddc8d76ff3df32dc18aaf
Tomoharu Kawakubo <tomo.kawakubo@gmail.com>
2024-05-07 20:54:54 +0900

    2024年05月07日授業分

---
+++ JavaScriptHonkakuV3/ikegaya-tone/chapter02/string1.js
@@ -0,0 +1,7 @@
+// escapse sequence エスケープ・シーケンス
+let str = 'He\'s a "Good" Player!!!';
+console.log(str);
+str = "He's a \"Good\" Player!!!";
+console.log(str);
+
+console.log("わたしはJava\tを勉強\nしている。");
