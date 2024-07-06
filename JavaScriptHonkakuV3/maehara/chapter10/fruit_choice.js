3e0845e9b427b0b8d5554c5a7cf23345d003d41c
Tomoharu Kawakubo <tomo.kawakubo@gmail.com>
2024-07-06 17:31:34 +0900

    2024年07月06日授業分

--- JavaScriptHonkakuV3/maehara/chapter10/fruit_choice.js
+++ JavaScriptHonkakuV3/maehara/chapter10/fruit_choice.js
@@ -1,7 +1,35 @@
 document.addEventListener('DOMContentLoaded', () => {
     const fruits = {banana: 'バナナ', apple: 'リンゴ', grape: 'ブドウ', orange: 'オレンジ', strawberry: 'イチゴ'};
-
+    
     const myform = document.querySelector('#myform');
     
-    
+    const div_radio = document.createElement('div');
+    for (const en_name in fruits) {
+        const ja_name = fruits[en_name];
+        const label = document.createElement('label');
+        const radio = document.createElement('input')
+        radio.type = 'radio';
+        radio.name = 'fruit';
+        radio.value = en_name;
+        const text = document.createTextNode(ja_name);
+        label.append(radio);
+        label.append(text);
+        div_radio.append(label);
+    }
+    myform.append(div_radio);
+    // チェックボックス
+    const div_checkbox = document.createElement('div');
+    for (const en_name in fruits) {
+        const ja_name = fruits[en_name];
+        const label = document.createElement('label');
+        const radio = document.createElement('input')
+        radio.type = 'checkbox';
+        radio.name = 'fruit';
+        radio.value = en_name;
+        const text = document.createTextNode(ja_name);
+        label.append(radio);
+        label.append(text);
+        div_checkbox.append(label);
+    }
+    myform.append(div_checkbox);
 });
---
+++ JavaScriptHonkakuV3/maehara/chapter10/kekeibo_table.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const kakeibo_data = [
+        {date: '2024-07-03', category: '食費', memo: 'コーヒーを購入', income: 0, expense: 380},
+        {date: '2024-07-10', category: '給料', memo: '6月の給料', income: 280000, expense: 0},
+        {date: '2024-07-11', category: '教養娯楽費', memo: '書籍を購入', income: 0, expense: 380},
+        {date: '2024-07-14', category: '交際費', memo: '同期会の会費', income: 0, expense: 5000},
+        {date: '2024-07-18', category: '水道光熱費', memo: '6月の電気代', income: 0, expense: 7680},
+    ];
+
+
+});
