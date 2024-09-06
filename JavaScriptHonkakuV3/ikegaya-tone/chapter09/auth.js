19b3fc955c92f6d562988c1425f976391e673847
Tomoharu Kawakubo <tomo.kawakubo@gmail.com>
2024-09-06 22:43:00 +0900

    2024年09月06日授業分

--- JavaScriptHonkakuV3/ikegaya-tone/chapter09/auth.js
+++ JavaScriptHonkakuV3/ikegaya-tone/chapter09/auth.js
@@ -1,7 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
     const auth_button = document.querySelector('#auth-button');
-    auth_button.disabled = true;
-    /*
-    ユーザ名、パスワードの両方が8文字以上の場合だけ、認証ボタンをクリックできるようにする
-    */
+
+    const style_button = document.querySelector('#style-button');
+    style_button.addEventListener('click', () => {
+        const divs = document.querySelectorAll('.container');
+        divs.forEach(div => div.classList.toggle('div-style'))
+    });
+
+    const username = document.querySelector('#username');
+    const password = document.querySelector('#password');
+
+    function enable_auth_button() {
+        if (username?.value?.trim().length >= 8
+         && password?.value?.trim().length >= 8) {
+            auth_button.disabled = false;
+         } else {
+            auth_button.disabled = true;
+         }
+    }
+
+    username.addEventListener('input', enable_auth_button);
+    password.addEventListener('input', enable_auth_button);
 });
