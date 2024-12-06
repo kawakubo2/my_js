31c9227c7578e9bc34a794ffac4a93537cccd2a0
Tomoharu Kawakubo <tomo.kawakubo@gmail.com>
2024-12-06 17:15:48 +0900

    2024年12月06日授業分

---
+++ JavaScriptHonkakuV3/suzuki_ikegaya/chapter02/var1.js
@@ -0,0 +1,12 @@
+let a; // メモリ領域を確保
+a = 234; // 代入
+console.log("a=" + a);
+
+let x = 123; // 初期化 --- 変数宣言 + 代入
+console.log("x=" + x);
+
+x = x + 100;
+console.log("x=" + x);
+
+const z = 1000; // 定数定義
+
