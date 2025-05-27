3de237952193945caf77b534bf5aad4632d4c004
Tomoharu Kawakubo <tomo.kawakubo@gmail.com>
2025-05-27 18:16:14 +0900

    2025年05月27日授業分

---
+++ JavaScriptHonkakuV3/ikegaya_minamiguchi/chapter06/20250527/fruits_counter.js
@@ -0,0 +1,13 @@
+/*
+フルーツ名が入った配列を受け取り、
+フルーツの個数を連想配列として
+返す関数
+
+['banana', 'apple', 'orange', 'banana', 'banana', 'grape', 'apple']
+   ↓
+{banana: 3, apple: 2, orange: 1, grape: 1}
+*/
+
+function fruits_counter(fruits) {
+    
+}
