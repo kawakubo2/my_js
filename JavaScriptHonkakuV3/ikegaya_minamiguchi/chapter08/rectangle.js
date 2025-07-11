b3c66e0be2e80b821999534bec9896236038456d
Tomoharu Kawakubo <tomo.kawakubo@gmail.com>
2025-07-11 22:33:26 +0900

    2025年07月11日授業分

--- JavaScriptHonkakuV3/ikegaya_minamiguchi/chapter08/rectangle.js
+++ JavaScriptHonkakuV3/ikegaya_minamiguchi/chapter08/rectangle.js
@@ -33,3 +33,40 @@ class Rectangle {
 const r1 = new Rectangle(8, 6);
 
 console.log(`幅が${r1.width}、高さが${r1.height}の長方形の面積は${r1.getArea()}`);
+
+class TopLeftRectangle extends Rectangle {
+    #x = 0.0;
+    #y = 0.0;
+    constructor(width, height, x, y) {
+        super(width, height);
+        this.x = x;
+        this.y = y;
+    }
+
+    get x() {
+        return this.#x;
+    }
+    set x(value) {
+        if (!(typeof value === 'number')) {
+            throw new Error('x座標は数値ではありません。');
+        }
+        this.#x = value;
+    }
+
+    get y() {
+        return this.#y;
+    }
+    set y(value) {
+        if (!(typeof value === 'number')) {
+            throw new Error('x座標は数値ではありません。');
+        }
+        this.#y = value;
+    }
+
+    getDistance() {
+        return Math.hypot(this.x, this.y);
+    }
+}
+
+const topLeftRec = new TopLeftRectangle(10, 20, 8, 6);
+console.log(`面積: ${topLeftRec.getArea()} 原点からの距離: ${topLeftRec.getDistance()}`);
