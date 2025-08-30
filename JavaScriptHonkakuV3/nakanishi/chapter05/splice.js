7f097fd36bffb6ded9a87f7635ef9a98d5dfa9b0
Tomoharu Kawakubo <tomo.kawakubo@gmail.com>
2025-08-30 22:06:28 +0900

    2025年08月30日授業分

--- JavaScriptHonkakuV3/nakanishi/chapter05/splice.js
+++ JavaScriptHonkakuV3/nakanishi/chapter05/splice.js
@@ -5,3 +5,12 @@ const result1 = array1.splice(2, 3);
 console.log('array1.splice(2, 3)');
 console.log(`splice後の配列: ${array1.toString()}`);
 console.log(`削除された要素: ${result1.toString()}`);
+
+console.log('--- 次回ここから ---');
+console.log('--- 置換 --');
+const array2 = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+console.log(`元の配列: ${array2.toString()}`);
+const result2 = array2.splice(2, 3, 'W', 'X', 'Y', 'Z');
+console.log("array2.splice(2, 3, 'W', 'X', 'Y', 'Z')");
+console.log(`splice後の配列: ${array2.toString()}`);
+console.log(`削除された要素: ${result2.toString()}`);
