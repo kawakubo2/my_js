3890ffa500463f72b01c3173321a3698263c5607
Tomoharu Kawakubo <tomo.kawakubo@gmail.com>
2025-09-19 21:36:36 +0900

    2025年09月19日授業分

--- JavaScriptHonkakuV3/ikegaya_minamiguchi/chapter00/books.js
+++ JavaScriptHonkakuV3/ikegaya_minamiguchi/chapter00/books.js
@@ -4,20 +4,22 @@ document.addEventListener('DOMContentLoaded', () => {
     const price = document.querySelector('#price');
     const books = document.querySelector('#bookshelf');
 
+    function create_tr() {
+        const tr = document.createElement('tr');
+        tr.appendChild(create_td(title.value));
+        tr.appendChild(create_td(publisher.value));
+        tr.appendChild(create_td(price.value));
+        return tr;
+    }
+    function create_td(value) {
+        const td = document.createElement('td');
+        td.textContent = value;
+        return td;
+    }
+
     document.querySelector('#bookshelf').addEventListener('click', () => {
         // TODO バリデーション
-        const td_title = document.createElement('td');
-        td_title.textContent = title.value;
-        const td_publisher = document.createElement('td');
-        td_publisher.textContent = publisher.value;
-        const td_price = document.createElement('td');
-        td_price.textContent = price.value; 
-
-        const tr = document.createElement('tr');
-        tr.appendChild(td_title);
-        tr.appendChild(td_publisher);
-        tr.appendChild(td_price);
-
+        const tr = create_tr();
         books.appendChild(tr);
     });
 });
--- JavaScriptHonkakuV3/ikegaya_minamiguchi/chapter09/bookshelf/bookshelf.js
+++ JavaScriptHonkakuV3/ikegaya_minamiguchi/chapter09/bookshelf/bookshelf.js
@@ -4,14 +4,59 @@ document.addEventListener('DOMContentLoaded', () => {
     const price = document.querySelector('#price');
     const error_summary = document.querySelector('#error-summary');
     const tbody = document.querySelector('#book-list');
+    const form = document.querySelector('#bookshelf-form');
 
     function isEmpty(value) {
         return value === null || value.trim() === '';
     }
     function validate() {
         const errors = [];
+        error_summary.textContent = '';
+        if (isEmpty(title.value)) {
+            errors.push('題名は必須入力です。');
+        }
+        if (isEmpty(publisher.value)) {
+            errors.push('出版社は必須入力です。');
+        }
+        if (isEmpty(price.value)) {
+            errors.push('価格は必須入力です。');
+        } else if (!(/^\d+$/.test(price.value))) {
+            errors.push('価格は0以上の整数値を入力してください。');
+        }
+        return errors;
+    }
+
+    function print_errors(errors) {
+        const fragment = document.createDocumentFragment();
+        for (const error of errors) {
+            const li = document.createElement('li');
+            li.textContent = error;
+            fragment.append(li);
+        }
+        error_summary.append(fragment);
+    }
+
+    function create_tr() {
+        const tr = document.createElement('tr');
+        tr.append(create_td(title.value));
+        tr.append(create_td(publisher.value));
+        tr.append(create_td(price.value));
+        return tr;
+    }
+
+    function create_td(value) {
+        const td = document.createElement('td');
+        td.textContent = value;
+        return td;
     }
     tbody.addEventListener('click', () => {
-
+        const errors = validate();
+        if (errors.length > 0) {
+            print_errors(errors);
+            return;
+        }
+        tbody.append(create_tr());
+        form.reset();
+        title.focus();
     });
 });
